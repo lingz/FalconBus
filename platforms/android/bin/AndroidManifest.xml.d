9398ac799d1610ba577235fcd8fbfe4d501850c5
lingz <lz781@nyu.edu>
2014-02-10 15:30:04 +0400

    removed platforms

--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/ling/Documents/falconBus/platforms/android/bin/AndroidManifest.xml : \
-/home/ling/Documents/falconBus/platforms/android/AndroidManifest.xml \
--- platforms/android/bin/HelloWorld-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/ling/Documents/falconBus/platforms/android/bin/HelloWorld-debug-unaligned.apk : \
-/home/ling/Documents/falconBus/platforms/android/bin/HelloWorld.ap_ \
-/home/ling/Documents/falconBus/platforms/android/bin/classes.dex \
-/home/ling/Documents/falconBus/platforms/android/libs/cordova-3.2.0.jar \
